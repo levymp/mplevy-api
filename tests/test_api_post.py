3fb37515625ad901fc1be639d8a7686f7669e720
Michael Patrick Levy <levymp@umich.edu>
2020-10-09 17:08:18 -0700

    updating test

--- tests/test_api_post.py
+++ tests/test_api_post.py
@@ -13,57 +13,58 @@ DIRECTORY_URL = BASE_URL + 'directory'
 
 # # open file
 
+def main():
+
+    file = open(os.path.realpath('../../../MBOT-RPI/data/convex_10mx10m_5cm.log'), 'rb')
+    file_payload = {'logfile': file}
+    params_payload = {'name': 'CLASS FILE', 'description': 'convex_10mx10m_5cm' }
+
+    # post file
+    r = requests.post(LOG_URL, files=payload, params=params_payload)
+
+    if r.status_code != 200:
+        print(r.text)
+        return -1
+
+    # get file
+
+    r = requests.get(LOG_URL, params={'runId': r['runId']})
+
+    # check response
+    if r.status_code != 200:
+        print(r.text)
+        return -1
+
+    file_path = Path('/tmp/mbot_int.pkl')
+    file_path.unlink(missing_ok=True)
+    # read in content
+    with open(file_path, 'wb') as fd:
+        fd.write(r.content)
+    # read in df
+    df = pd.read_pickle(file_path)
+    print(df.keys())
+
+    # get table
+    r = requests.get(DIRECTORY_URL, params={'database': 'prod'})
+
+    # check response
+    if r.status_code != 200:
+        print(r.text)
+        return -1
+
+    file_path = Path('/tmp/mbot_table.pkl')
+    file_path.unlink(missing_ok=True)
+    # read in content
+    with open(file_path, 'wb') as fd:
+        fd.write(r.content)
+    # read in df
+    df = pd.read_pickle(file_path)
+    print(df.keys())
+
+    # delete file
+    r = requests.delete(LOG_URL, params={'runId': r['runId']})
 
 
-file = open(os.path.realpath('../../../MBOT-RPI/data/convex_10mx10m_5cm.log'), 'rb')
-file_payload = {'logfile': file}
-params_payload = {'name': 'CLASS FILE', 'description': 'convex_10mx10m_5cm' }
-
-# post file
-r = requests.post(LOG_URL, files=payload, params=params_payload)
-
-if r.status_code != 200:
-    print(r.text)
-    return -1
-
-# get file
-
-r = requests.get(LOG_URL, params={'runId': r['runId']})
-
-# check response
-if r.status_code != 200:
-    print(r.text)
-    return -1
-
-file_path = Path('/tmp/mbot_int.pkl')
-file_path.unlink(missing_ok=True)
-# read in content
-with open(file_path, 'wb') as fd:
-    fd.write(r.content)
-# read in df
-df = pd.read_pickle(file_path)
-print(df.keys())
-
-# get table
-r = requests.get(DIRECTORY_URL, params={'database': 'prod'})
-
-# check response
-if r.status_code != 200:
-    print(r.text)
-    return -1
-
-file_path = Path('/tmp/mbot_table.pkl')
-file_path.unlink(missing_ok=True)
-# read in content
-with open(file_path, 'wb') as fd:
-    fd.write(r.content)
-# read in df
-df = pd.read_pickle(file_path)
-print(df.keys())
-
-# delete file
-r = requests.delete(LOG_URL, params={'runId': r['runId']})
-
-
-
+if __name__ == "__main__":
+    main()
 
